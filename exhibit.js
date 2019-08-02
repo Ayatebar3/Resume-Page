2a44f899d7a0091bcf1141f45022701b86e2d344
Ayatebar3 <aliatebar@yahoo.com>
2019-08-02 13:54:23 -0700

    First Push

---
+++ exhibit.js
@@ -0,0 +1,35 @@
+
+
+function readmore() {
+    
+    var containerDiv = document.getElementById("read_more_id");  
+    containerDiv.classList.remove("ReadMore");
+    containerDiv.classList.add("Read_more_hide"); 
+    
+	containerDiv = document.getElementById("web_content2");
+    containerDiv.classList.remove("content_2");
+    containerDiv.classList.add("content_2_shown");
+    
+    containerDiv = document.getElementById("footer");
+    containerDiv.classList.remove("footer_hide");
+    containerDiv.classList.add("footer_show");
+    
+    containerDiv = document.getElementById("skechImage_id");
+    containerDiv.style.display = "block";
+        	
+}
+
+function readless() {
+    
+	var containerDiv = document.getElementById("web_content2");
+    containerDiv.classList.remove("content_2_shown");
+    containerDiv.classList.add("content_2"); 
+    
+    containerDiv = document.getElementById("footer");
+    containerDiv.classList.remove("footer_show");
+    containerDiv.classList.add("footer_hide");
+    
+    containerDiv = document.getElementById("read_more_id");  
+    containerDiv.classList.remove("Read_more_hide");
+    containerDiv.classList.add("ReadMore"); 
+}
---
+++ readme.txt
@@ -0,0 +1,23 @@
+
+Student Name: Ali Atebar 
+SID: 915013263
+Partner: Nabil Frumoli
+ SID: 916153465
+
+
+
+
+Color:
+Light orange R: 249 G: 172 B: 120
+Dark orange for lettering R: 201 G: 82 B: 61
+
+Monserrat extra light
+<link href="https://fonts.googleapis.com/css?family=Montserrat:200,500" rel="stylesheet"> 
+font-family: montserrat, sans-serif; font-weight: 200; font-style: normal;
+
+Monserrat Medium
+<link href="https://fonts.googleapis.com/css?family=Montserrat:200,500" rel="stylesheet"> 
+font-family: montserrat, sans-serif; font-weight: 500; font-style: normal;
+
+Navigation menu/ title/ body
+Helvetica: font-family: helvetica;
